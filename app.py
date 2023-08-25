2cc6a1f1f971262ab022409c838b1ee81bfc42d0
MaryanNCanghe <132509932+MaryanNCanghe@users.noreply.github.com>
2023-08-25 21:04:11 +0000

    Changes
    added dynamic data and API route, dor next include the aplly page.

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "hello, Maria"
+JOBS =[
+  {
+    'id': 1,
+    'title':'Data Analyst',
+    'localização':' portugal, lisboa',
+    'salário':'1.000€'
+  },
+  {
+     'id': 2,
+    'title':'Data Analyst',
+    'localização':'frança, lyon',
+    'salário':'4.000€'
+  },
+  {
+   'id': 3,
+    'title':'web develoter',
+    'localização':'Norway, oslo',
+    'salário':'7.000€'
+ },
+   {
+   'id': 4,
+    'title':'web develoter',
+    'localização':'remote',
+    'salário':'10.000€'
+ },
+]
 
+
+@app.route("/")
+def hello_keyline():
+  return render_template('home.html', 
+                         jobs=JOBS, 
+                         company_name='keyline')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
